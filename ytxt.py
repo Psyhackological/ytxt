ccda14b0296407ef3fc52fc004a4264ced075c8b
Psyhackological <konradkon@duck.com>
2022-12-17 15:17:36 +0100

    Made pylint happy (8.66/10)

--- ytxt.py
+++ ytxt.py
@@ -1,16 +1,39 @@
-import yt_dlp
 import argparse
 import re
 import glob
 from pathlib import Path
+import sys
+
+import yt_dlp
 
 # Documentation: https://docs.python.org/3/library/argparse.html
 # Tutorial:      https://docs.python.org/3/howto/argparse.html#
 parser = argparse.ArgumentParser(prog="YouTube Video to TXT")
-parser.add_argument(nargs='*', type=str, help="YouTube Video URL (str)", dest="urls")
-parser.add_argument('-l', "--language", type=list, help="subtitles languages (default=['en']) (list)", dest="languages", default=["en", "en-GB"])
-parser.add_argument('-f', "--format", nargs='?', type=str, help="subtitles format (default='vtt') (str)", dest="format", default="vtt")
-parser.add_argument('-sl', "--sub-langs", action="store_true", help="prints all available subtitles languages", dest="print_langs")
+parser.add_argument(nargs="*", type=str, help="YouTube Video URL (str)", dest="urls")
+parser.add_argument(
+    "-l",
+    "--language",
+    type=list,
+    help="subtitles languages (default=['en']) (list)",
+    dest="languages",
+    default=["en", "en-GB"],
+)
+parser.add_argument(
+    "-f",
+    "--format",
+    nargs="?",
+    type=str,
+    help="subtitles format (default='vtt') (str)",
+    dest="format",
+    default="vtt",
+)
+parser.add_argument(
+    "-sl",
+    "--sub-langs",
+    action="store_true",
+    help="prints all available subtitles languages",
+    dest="print_langs",
+)
 args = parser.parse_args()
 
 args.urls = [r"https://youtu.be/F1Hq8eVOMHs"]
@@ -18,8 +41,8 @@ args.urls = [r"https://youtu.be/F1Hq8eVOMHs"]
 # ydl_opts: https://github.com/yt-dlp/yt-dlp/blob/master/yt_dlp/YoutubeDL.py#L180
 def show_subs_langs():
     ydl_opts = {  # YT-DLP OPTIONS
-        'listsubtitles': True,
-        'skip_download': True,
+        "listsubtitles": True,
+        "skip_download": True,
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -27,49 +50,49 @@ def show_subs_langs():
             for url in args.urls:
                 ydl.extract_info(url, download=False)
         except yt_dlp.utils.DownloadError:
-            quit()
+            sys.exit()
 
 
 def download_vtt_file():
     ydl_opts = {  # YT-DLP OPTIONS
-        'skip_download': True,
-        'writesubtitles': True,
-        'subtitleslangs': args.languages,
-        'subtitlesformat': args.format
+        "skip_download": True,
+        "writesubtitles": True,
+        "subtitleslangs": args.languages,
+        "subtitlesformat": args.format,
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         try:
-            error_code = ydl.download(args.urls)
+            ydl.download(args.urls)
 
         except yt_dlp.utils.DownloadError:
-            quit()
+            sys.exit()
 
 
 class SubtitleTXT:
     def __init__(self, vtt_file_name):
-        with open(f"{vtt_file_name}", 'rt') as vtt_file:
+        with open(f"{vtt_file_name}", "rt", encoding="utf-8") as vtt_file_handle:
             self.filename = Path(vtt_file_name).stem
-            self.file_text = vtt_file.read()
+            self.file_text = vtt_file_handle.read()
 
     def delete_not_needed_stuff(self):
         # regex = r"^00:00:\d{2}\.\d{3} --> 00:00:\d{2}\.\d{3}.*"
         regex = r"\d{2}:\d{2}:\d{2}\.\d{3} --> .+"
         match = re.search(regex, self.file_text)
         if match is not None:
-            self.file_text = self.file_text[match.start():]
+            self.file_text = self.file_text[match.start() :]
 
     def delete_newlines(self):
-        regex = r"(\n|&nbsp;|​)+"
-        self.file_text = re.sub(regex, ' ', self.file_text)
+        regex = r"(\n|&nbsp;|\u200B)+"
+        self.file_text = re.sub(regex, " ", self.file_text)
 
     def delete_timestamps(self):
         # regex = r"\d{2}:\d{2}:\d{2}\.\d{3} --> \d{2}:\d{2}:\d{2}\.\d{3}.*"
         regex = r"\d{2}:\d{2}:\d{2}(,|\.){1}\d{3} .+ \d{2}:\d{2}:\d{2}(,|\.){1}\d{3}"
-        self.file_text = re.sub(regex, '', self.file_text)
+        self.file_text = re.sub(regex, "", self.file_text)
 
     def make_file(self):
-        with open("clean.txt", "wt") as clean_txt:
+        with open("clean.txt", "wt", encoding="utf-8") as clean_txt:
             clean_txt.write(self.file_text)
 
     def clean_to_txt(self):
@@ -92,8 +115,8 @@ class SubtitleTXT:
 
 if args.print_langs is False:
     download_vtt_file()
-    vtt_files = glob.glob("*.vtt")
-    for vtt_file in vtt_files:
+    vtt_files_list = glob.glob("*.vtt")
+    for vtt_file in vtt_files_list:
         SubTxt = SubtitleTXT(vtt_file)
         SubTxt.clean_to_txt()
 else:
